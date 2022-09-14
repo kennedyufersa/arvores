5c513bbba65f7f2688ed7e8d15df84669a19abae
Kennedy Lopes <kennedy.lopes@ufersa.edu.br>
2022-09-14 20:51:49 -0300

    Inserindo novos elementos

--- arvoreBinaria.c
+++ arvoreBinaria.c
@@ -14,5 +14,18 @@ int main(int argc, char const *argv[])
   raiz->dir = 0;
   Aluno *a = criarAluno("Kennedy\0", 10, 8.5);
   raiz->aluno = *a;
+
+  Aluno *vetor[5];
+  vetor[0] = criarAluno("aluno11\0", 11, 9.9);
+  vetor[1] = criarAluno("aluno12\0", 11, 9.5);
+  vetor[2] = criarAluno("aluno13\0", 11, 9.0);
+  vetor[3] = criarAluno("aluno14\0", 11, 8.9);
+  vetor[4] = criarAluno("aluno15\0", 11, 7.9);
+
+  raiz->esq = (struct Node*)malloc(sizeof(struct Node));
+  raiz->esq->esq = 0;
+  raiz->esq->dir = 0;
+  raiz->esq->aluno = *vetor[0];
+
   return 0;
 }
