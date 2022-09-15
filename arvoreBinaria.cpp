75cc09f7299648ed25c32c6beb7970af7547e87b
kennedyufersa <73143709+kennedyufersa@users.noreply.github.com>
2022-09-14 21:46:20 -0300

    Update arvoreBinaria.cpp

---
+++ arvoreBinaria.cpp
@@ -0,0 +1,46 @@
+#include "Aluno.c"
+#include <stdio.h>
+
+struct Node {
+    Aluno aluno;
+    struct Node *esq;
+    struct Node *dir;
+};
+
+Node *novoNo(Aluno a) {
+    Node *aux = (Node *)malloc(sizeof(Node));
+    aux->aluno = criarAluno2(a.nome, a.Id, a.nota);
+    aux->esq = 0;
+    aux->dir = 0;
+    return aux;
+}
+
+Node *inserir(Node *r, Aluno a) {
+    if (r == 0) {
+        r = novoNo(a);
+    }
+    if (a.Id < r->aluno.Id) {
+        r->esq = inserir(r->esq, a);
+    } else if (a.Id > r->aluno.Id) {
+        r->dir = inserir(r->dir, a);
+    }
+    return r;
+}
+
+int main(int argc, char const *argv[]) {
+    Node *raiz = 0;
+
+    Aluno vetor[5];
+
+    vetor[0] = criarAluno2("A\0", 8, 8);
+    vetor[1] = criarAluno2("B\0", 4, 12);
+    vetor[2] = criarAluno2("C\0", 6, 13);
+    vetor[3] = criarAluno2("D\0", 12, 14);
+    vetor[4] = criarAluno2("E\0", 14, 15);
+
+    for (int i = 0; i < 5; i++) {
+        raiz = inserir(raiz, vetor[i]);
+    }
+
+    return 0;
+}
