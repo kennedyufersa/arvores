d152904950a04266b488b73a9431d52d848b4bf3
Kennedy <kennedy.lopes@ufersa.edu.br>
2022-09-28 09:52:03 -0300

    Separando a função lerArqvuio para servir apenas como uma função para recuperar os dados dos alunos

--- lerArquivos.cpp
+++ lerArquivos.cpp
@@ -1,17 +1,30 @@
+#include "Aluno.cpp"
 #include <stdio.h>
 
-int main(){
-    FILE *file = fopen("turma.dat", "r");
-    int nota;
-    char nome[80];
+Aluno *getAlunos(char *arquivo, int &quantidade) {
+    FILE *file = fopen(arquivo, "r");
+    Aluno *res = (Aluno *)malloc(50 * sizeof(Aluno));
+    float nota;
+    char *nome;
     int mat;
-
-    if(!file){
+    int i = 0;
+    Aluno aux;
+    if (!file) {
         printf("Impossível de abrir o arquivo");
     }
-    while(!feof(file)){
-        fscanf(file, "%d %d %s", &nota, &mat, nome);
-        printf("Nome: %s\nMatricula: %d\nNota:%d\n\n", nome, mat, nota);
+    while (!feof(file)) {
+        // fscanf(file, "%d %d %s", &nota, &mat, nome);
+        // printf("Nome: %s\nMatricula: %d\nNota:%d\n\n", nome, mat, nota);
+        nome = (char*)malloc(30*sizeof(char));
+        fscanf(file, "%f %d %s", &nota, &mat, nome);
+        aux = criarAluno2(nome, mat, nota);
+        res[i++] = aux;
     }
-
+    quantidade = i;
+    return res;
 }
+/*
+int main() {
+    char *arquivo = (char *)"turma.dat";
+    Aluno *alunos = getAlunos(arquivo);
+}*/
